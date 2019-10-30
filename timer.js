d3033e0d9244f02e31fd7c8331bfb622149045e5
Silkov Max <silkov34@yandex.ru>
2019-10-30 06:25:20 +0300

    hm2

--- timer.js
+++ timer.js
@@ -1,35 +1,76 @@
 'use strict'
-let contCurrent;
-let numberCount;
+let containerNowPosition;
+let containerNextPosition;
+let numberNow;
 let numberNext;
-let contFuture;
 let createElements = function() {
-    contCurrent = document.createElement("div");
-    contFuture = document.createElement("div");
-    numberCount = document.createElement("span");
+    containerNowPosition = document.createElement("div");
+    containerNextPosition = document.createElement("div");
+    numberNow = document.createElement("span");
     numberNext = document.createElement("span");
-    if (contCurrent && contFuture && numberCount && numberNext) {
-        contCurrent.classList.add("number");
-        contFuture.classList.add("number");
-        document.getElementsByClassName("field")[0].appendChild(contCurrent);
-        document.getElementsByClassName("field")[0].appendChild(contFuture);
+    if (containerNowPosition && containerNextPosition && numberNow && numberNext) {
+        containerNowPosition.classList.add("boxForNumber");
+        containerNextPosition.classList.add("boxForNumber");
+        document.getElementsByClassName("field")[0].appendChild(containerNowPosition);
+        document.getElementsByClassName("field")[0].appendChild(containerNextPosition);
 
-        numberCount.classList.add("numberCount");
-        contCurrent.appendChild(numberCount);
+        numberNow.classList.add("numberNow");
+        containerNowPosition.appendChild(numberNow);
 
         numberNext.classList.add("numberNext");
-        contFuture.appendChild(numberNext);
+        containerNextPosition.appendChild(numberNext);
 
-        contFuture.style.top = 200 + "px";
-        createNumbers("0", "1");
+        containerNextPosition.style.top = 200 + "px";
+        let D6=0;
+        let D6_1=1;
+
+        let MoveCur = setInterval(function changeDigit () {
+            createNumbers(D6, D6_1);
+            moveNumber(containerNowPosition, containerNextPosition);
+            if (D6_1<9) {
+                D6 ++;
+                D6_1 ++;
+                console.log(D6 + ' ' + D6_1);
+            } else {
+                D6 =0;
+                D6_1=1;
+            }
+        },1000);
+
 
 
     }
 
 }
 let createNumbers = function(a,b) {
-    numberCount.innerHTML = a;
+    numberNow.innerHTML = a;
     numberNext.innerHTML = b;
 }
 
 document.addEventListener("DOMContentLoaded", createElements);
+
+
+function moveNumber(digitCur, digitNext) {
+    console.log(digitCur);
+    let coefSpeed = 0.05;
+    let speedMsInteval = 50;
+    let digitCurTopPosition = 0; //текущая позиция, запоминание
+
+    let timer = setTimeout(function positionNumber() {
+        digitCurTopPosition -= coefSpeed * digitCur.offsetHeight;
+
+        digitCur.style.top = digitCurTopPosition + 'px';
+        digitNext.style.top = digitCurTopPosition + digitNext.offsetHeight + 'px';
+
+
+        if (digitCurTopPosition > -digitCur.offsetHeight) {
+            timer = setTimeout(positionNumber,speedMsInteval);
+
+
+        } else {
+            digitCur.style.top = "0px";
+            digitNext.style.top = digitNext.offsetHeight + 'px';
+        };
+
+    }, speedMsInteval);
+}
