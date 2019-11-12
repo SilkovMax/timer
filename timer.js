c649bf43f6ee30b4eae8d1a84d783c5f67b3d507
Silkov Max <silkov34@yandex.ru>
2019-11-12 22:16:44 +0300

    hm4

--- timer.js
+++ timer.js
@@ -1,31 +1,65 @@
 'use strict'
-let containerNowPosition;
-let containerNextPosition;
-let numberNow;
-let numberNext;
+let numbers = [];
+let fields = [];
+let containers = [];
+
+
+
+
+
 let createElements = function() {
     try{
-        containerNowPosition = document.createElement("div");
-        containerNextPosition = document.createElement("div");
-        numberNow = document.createElement("span");
-        numberNext = document.createElement("span");
-        if (containerNowPosition && containerNextPosition && numberNow && numberNext) {
-            containerNowPosition.classList.add("boxForNumber");
-            containerNextPosition.classList.add("boxForNumber");
-            document.getElementsByClassName("field")[0].appendChild(containerNowPosition);
-            document.getElementsByClassName("field")[0].appendChild(containerNextPosition);
+        for (let i=0; i<6; i++) {
+            fields.push(document.createElement("div"));
+            fields[i].classList.add("field");
+            document.getElementsByClassName("container")[0].appendChild(fields[i]);
+            for (let z=0; z<2; z++) {
+                containers.push(document.createElement("div"));
+                numbers.push(document.createElement("span"));
+                containers[z].classList.add("boxForNumber");
+                numbers[z].classList.add("number");
+                fields[i].appendChild(containers[z]);
+                containers[z].appendChild(numbers[z]);
+                if (z == 1) containers[z].style.top = 200 + "px";
 
-            numberNow.classList.add("numberNow");
-            containerNowPosition.appendChild(numberNow);
-
-            numberNext.classList.add("numberNext");
-            containerNextPosition.appendChild(numberNext);
-
-            containerNextPosition.style.top = 200 + "px";
-            moveNumber(containerNowPosition, containerNextPosition);
-
+            }
 
         }
+
+
+        if (fields && containers && numbers) {
+            document.getElementsByClassName("containers")[0].appendChild(f);
+            document.getElementsByClassName("field5")[0].appendChild(container5Position);
+            document.getElementsByClassName("field5")[0].appendChild(container5_1Position);
+
+            number5.classList.add("number5");
+
+
+            number5_1.classList.add("number5_1");
+            container5_1Position.appendChild(number5_1);
+
+            container5_1Position.style.top = 200 + "px";
+        }
+
+        if (field6 && container6Position && container6_1Position && number6 && number6_1) {
+            field6.classList.add("field6");
+            document.getElementsByClassName("container")[0].appendChild(field6);
+
+            container6Position.classList.add("boxForNumber");
+            container6_1Position.classList.add("boxForNumber");
+            document.getElementsByClassName("field6")[0].appendChild(container6Position);
+            document.getElementsByClassName("field6")[0].appendChild(container6_1Position);
+
+            number6.classList.add("number6");
+            container6Position.appendChild(number6);
+
+            number6_1.classList.add("number6_1");
+            container6_1Position.appendChild(number6_1);
+
+            container6_1Position.style.top = 200 + "px";
+        }
+        moveNumber();
+
     } catch(e) {
         let x = e.message;
         console.log(x);
@@ -33,51 +67,59 @@ let createElements = function() {
     }
 }
 
-let setNumbers = function(a,b) {
-    numberNow.innerHTML = a;
-    numberNext.innerHTML = b;
+let setNumbers = function(a,b,c,d) {
+    number6.innerHTML = a;
+    number6_1.innerHTML = b;
+    number5.innerHTML = c;
+    number5_1.innerHTML = d;
+
+
 }
 
 document.addEventListener("DOMContentLoaded", createElements);
 
 
-function moveNumber(digitCur, digitNext) {
+function moveNumber() {
     console.log(digitCur);
     let coefSpeed = 0.05;
     let speedMsInteval = 50;
-    let digitCurTopPosition = 0; //текущая позиция, запоминание
-    let D6=0;
-    let D6_1=1;
-    let D5=0;
-    let D5_1=1;
-    let D4=0;
-    let D4_1=1;
-    let D3=0;
-    let D3_1=1;
-    let D2=0;
-    let D2_1=1;
-    let D1=0;
-    let D1_1=1;
-    setNumbers(D6, D6_1);
+    let digitCurTopPosition6 = 0; //текущая позиция, запоминание
+    let digitCurTopPosition5 = 0; //текущая позиция, запоминание
+
+    let D = [0,1,0,1,0,1,0,1,0,1,0,1];
+
+    for (i in D) {
+        setNumber(D[i], number[i] }
+
+    setNumbers(D6, D6_1, D5, D5_1);
 
     let timer = setTimeout(function positionNumber() {
-        digitCurTopPosition -= coefSpeed * digitCur.offsetHeight;
-        digitCur.style.top = digitCurTopPosition + 'px';
-        digitNext.style.top = digitCurTopPosition + digitNext.offsetHeight + 'px';
-        if (digitCurTopPosition > -digitCur.offsetHeight) {
+        digitCurTopPosition6 -= coefSpeed * number6.offsetHeight;
+        if (D6 == 9) {
+            digitCurTopPosition5 -= coefSpeed * number5.offsetHeight;
+            number5.style.top = digitCurTopPosition5 + 'px';
+            number5_1.style.top = digitCurTopPosition5 + number5_1.offsetHeight + 'px';
+        };
+
+        number6.style.top = digitCurTopPosition6 + 'px';
+        number6_1.style.top = digitCurTopPosition6 + number6_1.offsetHeight + 'px';
+        if (digitCurTopPosition6 > -number6.offsetHeight) {
             timer = setTimeout(positionNumber,speedMsInteval);//step up
         } else {
             if (D6_1<9) {
                 D6<9 ? D6 ++ : D6=0;
                 D6_1 ++;
+
             } else {
                 D6 =9;
                 D6_1=0;
+                D5 ++;
+                D5_1 ++;
             }
-            setNumbers(D6, D6_1);
-            digitCur.style.top = "0px";
-            digitNext.style.top = digitNext.offsetHeight + 'px';
-            digitCurTopPosition = 0;
+            setNumbers(D6, D6_1, D5, D5_1);
+            number6.style.top = "0px";
+            number6_1.style.top = number6_1.offsetHeight + 'px';
+            digitCurTopPosition6 = 0;
             timer = setTimeout(positionNumber,speedMsInteval);
         }
 
